a614d9add86f10fd42b38e8fc653f4eb08e14fbc
alejandro cisneros <alex.cisnerosap@gmail.com>
2026-01-18 22:21:07 +0100

    first commit

--- app/src/main/java/com/example/filmoteca/Api/SeriesApi.java
+++ app/src/main/java/com/example/filmoteca/Api/SeriesApi.java
@@ -1,9 +1,11 @@
 package com.example.filmoteca.Api;
 
+import com.example.filmoteca.Model.Serie;
 import com.example.filmoteca.Response.SerieResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface SeriesApi {
@@ -12,6 +14,13 @@ public interface SeriesApi {
     Call<SerieResponse> getSeries(
             @Query("language") String language,
             @Query("page") int page
+    );
+    @GET("tv/{series_id}")
+    Call<Serie> getSerieDetail(
+            @Path("series_id") int seriesId,
+            @Query("api_key") String apiKey,
+            @Query("language") String language,
+            @Query("append_to_response") String appendToResponse // Usaremos "videos"
     );
 
 }
